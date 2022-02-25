a409e9fb6a7a1a22f60e2f1892101cb9855531d2
Ruslan Tumasoff <Rus-nura@yandex.ru>
2022-02-25 23:17:42 +0700

    Reconfigured WebSecurityConfig

--- src/main/java/ru/tumasoff/keycloakdemo/config/WebSecurityConfig.java
+++ src/main/java/ru/tumasoff/keycloakdemo/config/WebSecurityConfig.java
@@ -1,19 +1,23 @@
 package ru.tumasoff.keycloakdemo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.cors()
+    http
+      .cors()
       .and()
-      .authorizeRequests()
-      .anyRequest()
-      .authenticated()
-      .and()
+      .authorizeRequests(auth -> auth
+        .antMatchers(HttpMethod.GET, "/anonymous/**").permitAll()
+        .anyRequest()
+        .authenticated())
       .oauth2ResourceServer()
       .jwt();
   }
