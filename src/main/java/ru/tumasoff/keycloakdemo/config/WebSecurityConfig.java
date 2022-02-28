0aea7a7b18d815ab650c4e2501f6ab4d9e4220c6
Ruslan Tumasoff <Rus-nura@yandex.ru>
2022-02-28 23:22:49 +0700

    Opaque token: Configured WebSecurityConfig

--- src/main/java/ru/tumasoff/keycloakdemo/config/WebSecurityConfig.java
+++ src/main/java/ru/tumasoff/keycloakdemo/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.tumasoff.keycloakdemo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,6 +10,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Value("${spring.security.oauth2.resourceserver.opaque.introspection-uri}")
+  String introspectionUri;
+
+  @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-id}")
+  String clientId;
+
+  @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-secret}")
+  String clientSecret;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
@@ -18,7 +28,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers(HttpMethod.GET, "/anonymous/**").permitAll()
         .anyRequest()
         .authenticated())
-      .oauth2ResourceServer()
-      .jwt();
+      .oauth2ResourceServer(oauth2 ->
+        oauth2.opaqueToken(token -> token
+          .introspectionUri(this.introspectionUri)
+          .introspectionClientCredentials(this.clientId, this.clientSecret)));
   }
 }
